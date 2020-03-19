dd2e8bcb92411b67b33bf3e63734e4416bf4280c
qjrincoywork <quirjohnincoy.work@gmail.com>
2020-03-19 16:55:36 +0800

    footer changes

--- lib/view.php
+++ lib/view.php
@@ -6,13 +6,13 @@ class View
     public function view($filename, $data = null) {
         if(Session::getSession('User')) {
             require_once 'app/view/layouts/header.php';
+            require_once "app/view/". $filename. ".php";
+            require_once 'app/view/layouts/footer.php';
         } else {
             require_once 'app/view/layouts/loginHeader.php';
+            require_once "app/view/". $filename. ".php";
+            require_once 'app/view/layouts/loginFooter.php';
         }
-        require_once "app/view/". $filename. ".php";
-        require_once 'app/view/layouts/footer.php';
     }
 }
-
-
 ?>
